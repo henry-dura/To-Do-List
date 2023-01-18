b0306891829747e4a4c5fbef49b4623e06340b8a
henry-dura <henryikemefuna@gmail.com>
2023-01-18 16:27:25 +0100

    Add more display content from JS and style it.

--- src/modules/DisplayInHtml.js
+++ src/modules/DisplayInHtml.js
@@ -9,12 +9,16 @@ const displayInHtml = ()=>{
     const div = document.createElement("div");
     div.classList.add("list-block");
     div.innerHTML = `
-    <div class="list-line">
-    <input type="checkbox"  name="${list[list.length-1].description}" value="${list[list.length-1].description}">
-    <p>${list[list.length-1].description}</p>
+    <div class="full-list-line">
+        <div class="list-line">
+            <input type="checkbox"  name="${list[list.length-1].description}" value="${list[list.length-1].description}">
+            <p>${list[list.length-1].description}</p>
+        </div>
+        <i class="fa-solid fa-ellipsis-vertical"></i>
     </div>
     <hr>`;
     listHolder.appendChild(div);
 }
 
 export default displayInHtml;
+
